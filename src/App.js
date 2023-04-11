dc618d624a8d54372989743c4233940da72999f2
RyotaIwahashi <r-iwahashi@groovenauts.jp>
2023-04-11 10:06:18 +0900

    npm install react-query ライブラリ関数をアプリケーション全体に渡すためにindex.jsにproviderを設定

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-// このアプリケーションでnotes backendからデータを取得するには、
+// このアプリケーションでnotes backendでデータを作成する際には、
 // js-application 側のフロントエンドを一回起動して、ログイン処理して
 // localStorage に ユーザ情報や token を保持しておく必要がある。
 import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const baseUrl = '/api/notes'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
+  console.log(response.data)
   return response.data
 }
 getAll() // これでbackendにリクエストは飛んでることを確認できた。
 
 const App = () => {
+  const [user, setUser] = useState(null)
+  // const [token, setToken] = useState('')
+
+  useEffect(() => {
+    // アクセスの度にlocalStorageからログイン情報を読み込む
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      // setToken(user.token)
+    }
+  }, [])
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
@@ -27,6 +42,11 @@ const App = () => {
 
   return(
     <div>
+      {user &&
+        <div>
+          <p>{user.name} logged in</p>
+        </div>
+      }
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
         <input name="note" />
